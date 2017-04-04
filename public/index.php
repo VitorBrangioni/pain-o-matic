18f3143f13f021a95636010c0151eb4d3fc2102d
vitorbrangioni <vitorbrangioni@vitorbrangioni-HP-G42>
2017-04-03 21:13:22 -0300

    Preparando estrutura do projeto

---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+include_once '../view/site/home.php';
+?>
---
+++ view/site/interno/PainDiagram.php
@@ -0,0 +1,8 @@
+<?php 
+	include_once '../global-includes/head.html';
+?>
+<body>
+	<div class="container">
+		<h1>ola</h1>
+	</div>
+</body>
