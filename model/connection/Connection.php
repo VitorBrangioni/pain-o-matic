fae180894ef5527f742ff1cfac3ad9e156903ccd
vitor.brangioni <vitor.brangioni@SQEST125.squadra.com.br>
2017-04-10 20:21:44 -0300

    Finalized integration with the database and backend code.

--- model/connection/Connection.php
+++ model/connection/Connection.php
@@ -1,11 +1,16 @@
 <?php
 
+/**
+ * 
+ * @author vitor.brangioni
+ *
+ */
 class Connection
 {
-    private static $instance;
-    private static $dsn = "";
-    private static $user = "";
+    private static $dsn = "mysql:host=localhost;dbname=pain_o_matic;port-;charset=utf8";
+    private static $user = "root";
     private static $password = "";
+    private static $instance;
     
     private function __construct()
     {
@@ -13,9 +18,15 @@ class Connection
     
     public static function getInstance()
     {
-    	if (isset(self::$instance)) {
-    		self::$instance = new PDO($this->dsn, $this->user, $this->password);
+    	if (!isset(self::$instance)) {
+    		try {
+    			self::$instance = new PDO("mysql:host=localhost;dbname=pain_o_matic;port-;charset=utf8", "root", "");
+    		} catch (PDOException $e) {
+    			echo $e->getMessage();
+    		}
     	}
     	return self::$instance;
     }
 }
+
+Connection::getInstance();
