f74fa06deb6209033c54ca003df5dbb4c56d2c4e
vitor.brangioni <vitor.brangioni@SQEST125.squadra.com.br>
2017-04-26 19:55:32 -0300

    Created abstract class Pojo for stay generic

---
+++ model/pojo/Pojo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace model\pojo;
+
+require_once  '../../vendor/autoload.php';
+
+abstract class Pojo
+{
+	private $id;
+	
+	public function getId()
+	{
+		return $this->id;
+	}
+	
+	public function setId($id)
+	{
+		if ($id == null) {
+			throw new InvalidArgumentException();
+		}
+		$this->id = $id;
+	}
+}
