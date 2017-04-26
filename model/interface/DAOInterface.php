82e2296d05c94fc5b58a176bf3c93765af8be30c
vitor.brangioni <vitor.brangioni@SQEST125.squadra.com.br>
2017-04-26 19:57:20 -0300

    Conclusion of the interface DAO

--- model/interface/DAOInterface.php
+++ model/interface/DAOInterface.php
@@ -2,11 +2,24 @@
 
 namespace model\interfaces;
 
+require_once  '../../vendor/autoload.php';
+
+use model\pojo\Pojo;
+
 interface DAOInterface
 {
-	public function findById($id);
-	
-	public function edit();
+	public static function getInstance();
 	
 	public function listAll();
+	
+	public function findById($id);
+	
+	public function findGeneric($fiel, $value);
+	
+	public function insert(Pojo $pojo);
+	
+	public function edit(Pojo $pojo);
+	
+	public function delete(Pojo $pojo);
+	
 }
