e9297a9dfbc0d7528f3143fc6b2ac2b78b76a4ae
vitorbrangioni <vitorbrangioni@vitorbrangioni-p7-1060br>
2017-04-27 11:41:06 -0300

    database password altered

--- model/connection/Connection.php
+++ model/connection/Connection.php
@@ -13,7 +13,7 @@ class Connection implements ConnectionInterface
 {
     private static $dsn = "mysql:host=localhost;dbname=pain_o_matic;port-;charset=utf8";
     private static $user = "root";
-    private static $password = "";
+    private static $password = "root";
     private static $instance;
     
     private function __construct()
@@ -24,7 +24,7 @@ class Connection implements ConnectionInterface
     {
     	if (!isset(self::$instance)) {
     		try {
-    			self::$instance = new \PDO("mysql:host=localhost;dbname=pain_o_matic;port-;charset=utf8", "root", "");
+    			self::$instance = new \PDO("mysql:host=localhost;dbname=pain_o_matic;port-;charset=utf8", "root", "root");
     		} catch (PDOException $e) {
     			echo $e->getMessage();
     		}
