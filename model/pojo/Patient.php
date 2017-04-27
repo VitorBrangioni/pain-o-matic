7bcfae1e19bd2144df2301f7185634f81f05d1a3
vitorbrangioni <vitorbrangioni@vitorbrangioni-p7-1060br>
2017-04-27 11:40:04 -0300

    Implemented Patient pojo

--- model/pojo/Patient.php
+++ model/pojo/Patient.php
@@ -1,3 +1,59 @@
 <?php
 
 namespace model\pojo;
+
+/**
+ * 
+ * @author vitor.brangioni
+ *
+ */
+class Patient extends Pojo
+{
+	private $id;
+	private $name;
+	private $cpf;
+	private $rg;
+	private $photo;
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName(String $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(int $cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    public function getRg()
+    {
+        return $this->rg;
+    }
+
+    public function setRg(String $rg)
+    {
+        $this->rg = $rg;
+    }
+
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(String $photo)
+    {
+        $this->photo = $photo;
+    }
+
+}
