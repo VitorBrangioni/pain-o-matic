b208bcc7b31d24fc66a497aaf9cddea0e225855f
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-04-25 16:42:14 -0300

    test to global config

--- config/config.php
+++ config/config.php
@@ -1,5 +1,42 @@
 <?php
 
+const PRODUCTION = false;
+
+if (PRODUCTION === true) {
+    define("BASE_URI", "");
+    define("BASE_URL", 100);
+} elseif (PRODUCTION === false) {
+    define('BASE_URI', '/var/www/html/pain-o-matic/');
+    define('BASE_URL', 'localhost/');
+}
+
+const DIR_IMG = BASE_URL.'view/images/';
+const DIR_VIEW = BASE_URL.'view/internal/';
+const DIR_DIAGRAMS = DIR_IMG.'diagrams/';
+const DIR_PATIENT_PROFILE = '';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const BASE_URI = '/var/www/html/pain-o-matic/';
 const BASE_URL = 'localhost/pain-o-matic/';
 const DIR_IMG = BASE_URL.'view/images/';
@@ -11,3 +48,13 @@ $step1 = 'disabled';
 $step2 = 'disabled';
 $step3 = 'disabled';
 $step4 = 'disabled';
+
+
+
+if (PRODUCTION === true) {
+    // add url de producao
+} elseif (PRODUCTION === false) {
+    // add urls localhost
+}
+
+
