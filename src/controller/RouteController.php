58b7cb8bd140d1b2dea997e02f055033c1e1050d
Vitor Brangioni <vitorh.brangioni@gmail.com>
2018-05-03 22:05:54 -0300

    Implemented redirect method to use error message

--- src/controller/RouteController.php
+++ src/controller/RouteController.php
@@ -2,14 +2,18 @@
 
 namespace src\controller;
 
+use src\app\UserMessage;
 use src\controller\ScopeController;
 
 abstract class RouteController {
 
-    public static function redirect(string $viewName, array $scope = null)
+    public static function redirect(string $viewName, array $scope = null, UserMessage $userMessage = null)
     {
         if ($scope !== null) {
             ScopeController::getInstance()->add($scope);
+        }
+        if ($userMessage != null) {
+            $userMessage->add();
         }
         header("Location: ".$viewName.".php");
     }
